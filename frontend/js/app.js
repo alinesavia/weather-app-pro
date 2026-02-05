fbe2ac741d5b9578bdb24470cd4b75efa84a3bb8
Aline <alinelima@aluno.uespi.br>
2026-02-05 20:35:39 -0300

    integração com a API

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -6,21 +6,32 @@ const wind = document.querySelector("#wind");
 const humidity = document.querySelector("#humidity");
 const fellsLike = document.querySelector("#feels-like");
 const uvIndex = document.querySelector("#uv-index");
+const cityInput = document.getElementById("city-input");
+const searchBtn = document.getElementById("search-btn");
   
  
 
-  function updateWeather(data) {
-    cityName.textContent = data.city;
-    updateTime.textContent = `Atualizado às ${data.updatedAt}`;
-    temperature.textContent = `${data.temperature}°C`;
-    description.textContent = data.description;
-    wind.textContent = `${data.wind} km/h`;
-    humidity.textContent = `${data.humidity}%`;
-    fellsLike.textContent = `${data.feelsLike}°C`;
-    uvIndex.textContent = data.uvIndex;
-  }
+ function updateWeather(data) {
+  cityName.textContent = data.name;
 
-function getFakeWeatherData (city) {
+  const now = new Date();
+  updateTime.textContent = `Atualizado às ${now.getHours()}:${now
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}`;
+
+  temperature.textContent = `${Math.round(data.main.temp)}°C`;
+  description.textContent = data.weather[0].description;
+
+  wind.textContent = `${data.wind.speed} km/h`;
+  humidity.textContent = `${data.main.humidity}%`;
+  fellsLike.textContent = `${Math.round(data.main.feels_like)}°C`;
+
+  weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+}
+
+
+/*function getFakeWeatherData (city) {
   return {
     city: city,
     temperature: 32,
@@ -35,7 +46,47 @@ function getFakeWeatherData (city) {
 function loadWeather(city) {
   const weatherData = getFakeWeatherData(city);
   updateWeather(weatherData);
+}*/
+
+async function handleSearch() {
+  const city = cityInput.value.trim();
+
+  if (city === "") {
+    alert("Digite o nome de uma cidade");
+    return;
+  }
+
+  const data = await fetchWeather(city);
+  if (data) {
+    updateWeather(data);
+  }
+
+  cityInput.value = "";
 }
+
+const API_KEY = "af5fd9833d2c9fe91c037034e4d5dcde";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+
+async function fetchWeather(city) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric&lang=pt_br`
+    );
+
+    if (!response.ok) {
+      throw new Error("Cidade não encontrada");
+    }
+
+    const data = await response.json();
+    return data;
+
+  } catch (error) {
+    console.error(error);
+    alert("Erro ao buscar clima. Verifique o nome da cidade.");
+  }
+}
+
 
 document.addEventListener("DOMContentLoaded", () => {
   loadWeather("Teresina");
@@ -46,6 +97,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 300);
     }
 
+  searchBtn.addEventListener("click", handleSearch);
+
+  cityInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  });
 });
 
   
