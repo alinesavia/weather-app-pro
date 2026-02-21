a454fd25585387dc67c3d6cfe87b6fd339730121
Aline <alinelima@aluno.uespi.br>
2026-02-21 18:45:55 -0300

    feito integração API e atualização por hora

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -5,9 +5,13 @@ const description = document.querySelector("#description");
 const wind = document.querySelector("#wind");
 const humidity = document.querySelector("#humidity");
 const fellsLike = document.querySelector("#feels-like");
-const uvIndex = document.querySelector("#uv-index");
 const cityInput = document.getElementById("city-input");
 const searchBtn = document.getElementById("search-btn");
+const weatherIcon = document.getElementById("weather-icon");
+const loadingMessage = document.getElementById("loading-message");
+const errorMessage= document.getElementById("error-message");
+const hourlyContainer = document.getElementById("hourly-container");
+const dailyContainer = document.getElementById("daily-container");
   
  
 
@@ -30,38 +34,87 @@ const searchBtn = document.getElementById("search-btn");
   weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 }
 
-
-/*function getFakeWeatherData (city) {
-  return {
-    city: city,
-    temperature: 32,
-    description: "Ensolarado",
-    wind: 12,
-    humidity: 55,
-    uvIndex: 9,
-    updatedAt: "14:30"
-  };
+function showLoading() {
+  loadingMessage.classList.remove("hidden");
+  errorMessage.classList.add("hidden");
 }
 
-function loadWeather(city) {
-  const weatherData = getFakeWeatherData(city);
-  updateWeather(weatherData);
-}*/
+function hideLoading() {
+  loadingMessage.classList.add("hidden");
+}
+
+function showError(message) {
+  errorMessage.textContent = message;
+  errorMessage.classList.remove("hidden");
+}
+
+function updateHourForecast(data) {
+  hourlyContainer.innerHTML = "";
+
+  const nextHours = data.list.slice(0, 6);
+
+  nextHours.forEach(item => {
+    const card = document.createElement("div");
+    card.classList.add("hour-card");
+
+    const hour = new Date(item.dt_txt).getHours();
+    const icon = item.weather[0].icon;
+    const temp = Math.round(item.main.temp);
+
+    card.innerHTML = `
+    <p>${hour}h</p>
+    <img src="https://openweathermap.org/img/wn/${icon}.png" />
+    <p>${temp}°C</p>
+    `
+
+    hourlyContainer.appendChild(card);
+    
+  })
+    
+}
+
+async function fetchForecast(city) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric&lang=pt_br`
+    );
+
+    if (!response.ok) {
+      throw new Error("Erro ao buscar previsão");
+    }
+
+    const data = await response.json();
+    return data;
+
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
 
 async function handleSearch() {
   const city = cityInput.value.trim();
 
   if (city === "") {
-    alert("Digite o nome de uma cidade");
+    showError("Digite o nome de uma cidade");
     return;
   }
 
-  const data = await fetchWeather(city);
-  if (data) {
-    updateWeather(data);
-  }
+  showLoading();
 
-  cityInput.value = "";
+  const weatherData = await fetchWeather(city);
+  const forecastData = await fetchForecast(city);
+
+  hideLoading();
+
+  if (weatherData && forecastData) {
+    updateWeather(weatherData);
+    updateHourForecast(forecastData);
+  } else {
+    showError("Cidade não encontrada");
+  }
+ 
+    cityInput.value = "";
 }
 
 const API_KEY = "af5fd9833d2c9fe91c037034e4d5dcde";
@@ -82,15 +135,15 @@ async function fetchWeather(city) {
     return data;
 
   } catch (error) {
-    console.error(error);
-    alert("Erro ao buscar clima. Verifique o nome da cidade.");
+    return null;
   }
 }
 
 
+
 document.addEventListener("DOMContentLoaded", () => {
-  loadWeather("Teresina");
-  const card = document.querySelector(".weathercard");
+  
+  const card = document.querySelector(".weather-card");
   if (card) {
     setTimeout(() => {
       card.classList.add("show");
